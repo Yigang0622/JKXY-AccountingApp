a4e45c8bea04fa0afdf60be7f827ca0bebce2662
Mike.Zhou <zhou@zygmail.com>
2017-06-14 14:07:17 +0800

    TIckerView

--- app/src/main/java/com/jikexueyuan/accountingt/MainActivity.java
+++ app/src/main/java/com/jikexueyuan/accountingt/MainActivity.java
@@ -2,13 +2,30 @@ package com.jikexueyuan.accountingt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.robinhood.ticker.TickerUtils;
+import com.robinhood.ticker.TickerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TickerView tickerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new RecordBean();
+
+        tickerView = (TickerView) findViewById(R.id.amount_text);
+        tickerView.setCharacterList(TickerUtils.getDefaultNumberList());
+        tickerView.setText("1000");
+
+        tickerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tickerView.setText("2234");
+            }
+        });
+
     }
 }
