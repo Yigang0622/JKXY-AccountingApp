92af39f5de408778158ef4e5dc1fe4caf072f9b3
Mike.Zhou <zhou@zygmail.com>
2017-06-14 11:44:59 +0800

    RecordDatabaseHelper

--- app/src/main/java/com/jikexueyuan/accountingt/DateUtil.java
+++ app/src/main/java/com/jikexueyuan/accountingt/DateUtil.java
@@ -14,7 +14,7 @@ public class DateUtil {
     }
 
 
-    public static String getDateStr(){
+    public static String getFormattedDate(){
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         Date curDate = new Date(System.currentTimeMillis());//获取当前时间
         return formatter.format(curDate);
