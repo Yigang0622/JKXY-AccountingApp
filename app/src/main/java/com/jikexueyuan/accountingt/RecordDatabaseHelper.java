9e3a1ed2cad25a4d70b4298c295280aba3566e77
Mike.Zhou <zhou@zygmail.com>
2017-06-14 13:24:39 +0800

    RecordDatabaseHelper query

--- app/src/main/java/com/jikexueyuan/accountingt/RecordDatabaseHelper.java
+++ app/src/main/java/com/jikexueyuan/accountingt/RecordDatabaseHelper.java
@@ -2,9 +2,12 @@ package com.jikexueyuan.accountingt;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.LinkedList;
 
 public class RecordDatabaseHelper extends SQLiteOpenHelper {
 
@@ -63,6 +66,67 @@ public class RecordDatabaseHelper extends SQLiteOpenHelper {
         removeRecord(uuid);
         record.setUuid(uuid);
         addRecord(record);
+    }
+
+
+    public LinkedList<RecordBean> readRecords(String dateStr){
+
+        LinkedList<RecordBean> beanList = new LinkedList<RecordBean>();
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor;
+
+        cursor = db.rawQuery("select DISTINCT * from Record where date = ? order by time asc", new String[]{dateStr});
+
+        if (cursor.moveToFirst()) {
+            do {
+                String uuid = cursor.getString(cursor.getColumnIndex("uuid"));
+                int type = cursor.getInt(cursor.getColumnIndex("type"));
+                String category = cursor.getString(cursor.getColumnIndex("subtype"));
+                String remark = cursor.getString(cursor.getColumnIndex("remark"));
+                Double amount = cursor.getDouble(cursor.getColumnIndex("amount"));
+                String date = cursor.getString(cursor.getColumnIndex("date"));
+                long time = cursor.getLong(cursor.getColumnIndex("time"));
+
+                RecordBean bean = new RecordBean();
+                bean.setUuid(uuid);
+                bean.setType(type);
+                bean.setCategory(category);
+                bean.setRemark(remark);
+                bean.setAmount(amount);
+                bean.setDate(date);
+                bean.setTimeStamp(time);
+
+
+
+                beanList.add(bean);
+
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+
+        return beanList;
+    }
+
+    public LinkedList<String> readAvailableDate(){
+        LinkedList<String> list = new LinkedList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select DISTINCT * from Record order by date asc", new String[]{});
+
+        if (cursor.moveToFirst()){
+            do {
+                String date = cursor.getString(cursor.getColumnIndex("date"));
+
+                if (!list.contains(date)){
+                    list.add(date);
+                }
+
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
+        return list;
     }
 
 }
