64c7950036385aa9d918e47b62b2df7fd0134baa
Mike.Zhou <zhou@zygmail.com>
2017-06-15 09:18:53 +0800

    主界面layout

--- app/src/main/java/com/jikexueyuan/accountingt/MainActivity.java
+++ app/src/main/java/com/jikexueyuan/accountingt/MainActivity.java
@@ -2,6 +2,7 @@ package com.jikexueyuan.accountingt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.robinhood.ticker.TickerUtils;
@@ -16,16 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tickerView = (TickerView) findViewById(R.id.amount_text);
-        tickerView.setCharacterList(TickerUtils.getDefaultNumberList());
-        tickerView.setText("1000");
+        getSupportActionBar().setElevation(0);
 
-        tickerView.setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tickerView.setText("2234");
+                Log.d("TTT","1111");
             }
         });
+//
+//        tickerView = (TickerView) findViewById(R.id.amount_text);
+//        tickerView.setCharacterList(TickerUtils.getDefaultNumberList());
+//        tickerView.setText("1000");
+//
+//        tickerView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                tickerView.setText("2234");
+//            }
+//        });
 
     }
 }
